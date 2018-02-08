0feabee0ec1413430c027df8f9e06374d6fe0001
sunfei <sunfeiswag@gmail.com>
2018-02-09 04:26:28 +0800

    修复了一些错误

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -92,7 +92,7 @@ public class ChooseAreaFragment extends Fragment {
                         getActivity().finish();
                     }else if (getActivity() instanceof WeatherActivity){
                         WeatherActivity activity = (WeatherActivity)getActivity();
-                        activity.drawerLayout.closeDrawer();
+                        activity.drawerLayout.closeDrawers();
                         activity.swipeRefresh.setRefreshing(true);
                         activity.requestWeather(weatherId);
                     }
